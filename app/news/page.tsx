e8cf8576412124214117c32b8a7b381eb8fb22c2
munkhjinnext <munkhjin@nextstairs.com>
2025-03-26 16:44:56 +0900

    news section

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,34 +1,53 @@
 import { fetchPost } from "@/sanity/lib/fetchPost";
 import Image from "next/image";
 import { urlFor } from "@/sanity/imageUrl";
-import { PortableText } from "next-sanity";
 export default async function NewsPage() {
   const news = await fetchPost();
-
+  const getPlainText = (body: any) => {
+    return (
+      body
+        .filter((block: any) => block._type === "block") // Get only text blocks
+        .map((block: any) =>
+          block.children.map((child: any) => child.text).join(" "),
+        )
+        .join(" ") // Join all paragraphs
+        .slice(0, 102) + "..."
+    ); // Trim text
+  };
   return (
-    <div className="p-6 mt-10">
-      <h1 className="text-3xl font-bold">Latest News</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6">
+    <div className="p-6 mt-[140px]">
+      <h1 className="text-[64px] text-center font-bold">お知らせ</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6 p-20">
         {news.map((article: any) => (
-          <div key={article.slug.current} className="border p-4 rounded-lg">
+          <div
+            key={article.slug.current}
+            className="rounded-[10px] [box-shadow:10px_10px_15px_rgba(0,_0,_0,_0.1)]"
+          >
             <Image
-              src={urlFor(article.mainImage).width(300).height(200).url()}
+              src={urlFor(article.mainImage).width(800).height(600).url()}
               alt={article.title}
-              width={300}
-              height={200}
-              className="rounded-lg"
+              width={800}
+              height={400}
+              className="rounded-lg w-full object-cover h-auto"
             />
-            <h2 className="text-xl font-semibold mt-2">{article.title}</h2>
-            <p className="text-sm">
-              {new Intl.DateTimeFormat("ja-JP", {
-                year: "numeric",
-                month: "long",
-                day: "numeric",
-              }).format(new Date(article.publishedAt))}
-            </p>
-            {Array.isArray(article.body) && (
-              <PortableText value={article.body} />
-            )}
+            <div className="pt-10 pb-[30px] px-5 flex flex-col gap-5">
+              <p className="text-[15px] text-[#000000ff] leading-[2]">
+                {new Intl.DateTimeFormat("ja-JP", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                }).format(new Date(article.publishedAt))}
+              </p>
+              <h2 className="text-lg font-semibold text-[#001849ff] leading-[1.4]">
+                {article.title}
+              </h2>
+
+              {Array.isArray(article.body) && (
+                <p className="text-[#000000ff] leading-[2] text-[15px]">
+                  {getPlainText(article.body)}
+                </p>
+              )}
+            </div>
           </div>
         ))}
       </div>
--- components/Sections/NewsSection.tsx
+++ components/Sections/NewsSection.tsx
@@ -1,3 +1,27 @@
+import { fetchPost } from "@/sanity/lib/fetchPost";
+
 export default function NewsSection() {
-  return <section></section>;
+  const news = fetchPost();
+  return (
+    <section>
+      <div className="pt-[280px] pb-[140px] flex flex-col items-center">
+        <div className="w-[1280px] px-10">
+          <div>
+            <p className="text-[144px] font-poppins font-semibold text-[#f5f5f5] tracking-[-0.07em] leading-none">
+              NEWS
+            </p>
+          </div>
+          <div className="flex gap-[70px]">
+            <p
+              className="text-2xl font-medium pt-10"
+              style={{ writingMode: "vertical-rl" }}
+            >
+              新着情報
+            </p>
+            <div className="rounded-tl-[50px] rounded-bl-[50px] py-20 pl-20 pr-10 bg-[#f5f5f5] w-full"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
