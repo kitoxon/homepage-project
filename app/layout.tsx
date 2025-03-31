f6f9bab48e67f7c4eefe6da3879eee93a1be3e63
munkhjinnext <munkhjin@nextstairs.co.jp>
2025-03-31 16:23:45 +0900

    metadata added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,15 @@
-"use client";
-
-import Navbar from "@/components/Navbar";
 import "../styles/global.css";
 import { Noto_Sans_JP, Poppins } from "next/font/google";
-import Footer from "@/components/Footer";
-import { usePathname } from "next/navigation";
+import ClientLayout from "@/components/Global/ClientLayout";
+export const metadata = {
+  title: "株式会社NextStairs | ネクストステアーズ",
+  description:
+    "「インサイトを通じて、あらゆるスポーツの価値を高める」をミッションに掲げ、スポーツチームの日々のメディア露出を可視化するメディア露出価値換算サービス「Brand Insight(ブランドインサイト)」の開発、スポーツメディアSpoShipメディアの運営、営業支援事業をしています。",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
 const notoSansJP = Noto_Sans_JP({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
@@ -13,19 +18,16 @@ const poppins = Poppins({
   weight: ["400", "500", "600", "700"],
   subsets: ["latin"],
 });
+
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
-  const pathname = usePathname();
-  const isStudio = pathname?.startsWith("/studio");
+}) {
   return (
     <html lang="ja" className={`${notoSansJP.className}`}>
       <body>
-        {!isStudio && <Navbar />}
-        {children}
-        {!isStudio && <Footer />}
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
---
+++ components/Global/ClientLayout.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+export default function ClientLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+  const isStudio = pathname?.startsWith("/studio");
+
+  return (
+    <>
+      {!isStudio && <Navbar />}
+      {children}
+      {!isStudio && <Footer />}
+    </>
+  );
+}
